fc7aeac87492349eec208ce0e3939b05759acf55
tatyana-ketova <tatyana.i.ketova@gmail.com>
2023-11-07 15:40:33 +0100

    hide password

--- Create_DB.py
+++ Create_DB.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="31101987"
+  password=input('Right password')
 )
 
 #create a database if exists
